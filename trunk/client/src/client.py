291c9658bc4b348c698125a7c32f4ed36a3120bb
joao@setequatro.com.br <joao@setequatro.com.br@f6775974-6335-11de-8fc2-3351cd7166e9>
2009-08-21 05:05:09 +0000

    Camera

--- trunk/client/src/client.py
+++ trunk/client/src/client.py
@@ -1,7 +1,9 @@
+from characterControl.Camera import Camera
 import direct.directbase.DirectStart
 from characterControl import Player
 from worldControl import World
 
 if __name__ == '__main__':
-    p = Player()
+    c = Camera()
+    p = Player(c)
     run()
