692016ba9d4e20cdcb45cf2dd9e6dd5971491053
joao@setequatro.com.br <joao@setequatro.com.br@f6775974-6335-11de-8fc2-3351cd7166e9>
2009-08-23 04:45:03 +0000

    Camera

--- trunk/client/src/client.py
+++ trunk/client/src/client.py
@@ -1,7 +1,12 @@
 import direct.directbase.DirectStart
 from characterControl import Player
 from worldControl import World
+from pandac.PandaModules import WindowProperties
 
 if __name__ == '__main__':
+    props = WindowProperties()
+    props.setCursorHidden(True)
+    base.win.requestProperties(props)
+    
     p = Player()
     run()
