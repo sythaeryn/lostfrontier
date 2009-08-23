3f793479fc4b3c32d4456cc63dd4219820df9f46
joao@setequatro.com.br <joao@setequatro.com.br@f6775974-6335-11de-8fc2-3351cd7166e9>
2009-08-23 00:31:58 +0000

    Camera

--- trunk/client/src/characterControl/Player.py
+++ trunk/client/src/characterControl/Player.py
@@ -1,6 +1,7 @@
 from base.Person import Person
 from os import sep
 import yaml
+import math
 from characterControl.Camera import Camera
 
 # Pega as teclas do arquivo de configuracao
@@ -12,6 +13,10 @@ class Player(Person,object):
 
     smoth_mov = False
     camera = None
+    sensivity_x = 0.1
+    lastX = 0
+    lastY = 0
+    sensivity_y = 5
 
     def __init__(self):
         super(Player, self).__init__()
@@ -49,8 +54,11 @@ class Player(Person,object):
         if (self.state_key['aim'] == 0):
 
             if (self.smoth_mov):
+                self.state_key['speed_side'] = 0.8
                 self.camera.set_cam_pos(0.0,30.0,3.0)
                 self.smoth_mov = False
+                self.change_state('right', 0)
+                self.change_state('left', 0)
             else:
                 self.camera.cam_follow(30.0,3.0)
                 self.camera.look(self.person)
@@ -61,9 +69,29 @@ class Player(Person,object):
                 self.camera.set_cam_pos(-5.0,20.0,10.0)
                 self.smoth_mov = True
 
-            else:
+            else:                
+                mpos = base.mouseWatcherNode.getMouse()
                 self.camera.cam_follow(20.0,10.0,-5.0)
-        
+                if base.mouseWatcherNode.hasMouse():
+                    posx = mpos.getX()
+                    posy = mpos.getY()
+                    if (self.lastY != posy):
+                        base.camera.setP(base.camera.getP() + (posy - self.lastY) * self.sensivity_y)
+                        self.lastY = posy
+
+                    if (self.lastX != posx):
+                        self.state_key['speed_side'] = math.fabs(posx)
+                        if (posx > (0 + self.sensivity_x)):
+                            self.change_state('right', 1)
+                            self.change_state('left', 0)
+                        elif (posx < (0 - self.sensivity_x)):
+                            self.change_state('right', 0)
+                            self.change_state('left', 1)
+                        else:
+                            self.change_state('right', 0)
+                            self.change_state('left', 0)
+                        self.lastX = posx
+
         return task.cont
 
     def change_state(self, key, value):
--- trunk/client/src/characterControl/base/Person.py
+++ trunk/client/src/characterControl/base/Person.py
@@ -28,7 +28,7 @@ class Person(DirectObject):
         self.personActor.reparentTo(self.person)
         self.personActor.setPos(0,0,1.5)
         self.state_key = {'right':0, 'left':0,'jump': False, 'fall':0,
-                          'speed' : 0.8, 'up' : 0, 'down': 0, 'moving' : False,
+                          'speed_walk' : 0.8, 'speed_side' : 0.8,'up' : 0, 'down': 0, 'moving' : False,
                           'walk_right' : 0, 'walk_left' : 0, 'crouch' : 0, 'crouching' : False,'aim' : 0}
                           
         self.personActor.enableBlend()
@@ -48,17 +48,17 @@ class Person(DirectObject):
 
         #roda o sprite em seu propio eixo
         if (self.state_key['left']!=0):
-            self.person.setH(self.person.getH() + ((elapsed*300) * self.state_key['speed'] ))
+            self.person.setH(self.person.getH() + ((elapsed*300) * self.state_key['speed_side'] ))
         if (self.state_key['right']!=0):
-            self.person.setH(self.person.getH() - ((elapsed*300) * self.state_key['speed'] ))
+            self.person.setH(self.person.getH() - ((elapsed*300) * self.state_key['speed_side'] ))
         if (self.state_key['up']!=0):
-            self.person.setY(self.person, -((elapsed*25) * self.state_key['speed'] ))
+            self.person.setY(self.person, -((elapsed*25) * self.state_key['speed_walk'] ))
         if (self.state_key['down']!=0):
-            self.person.setY(self.person, +((elapsed*25) * self.state_key['speed'] ))
+            self.person.setY(self.person, +((elapsed*25) * self.state_key['speed_walk'] ))
         if (self.state_key['walk_right'] != 0):
-            self.person.setX(self.person, - ((elapsed*25) * self.state_key['speed'] ))
+            self.person.setX(self.person, - ((elapsed*25) * self.state_key['speed_walk'] ))
         if (self.state_key['walk_left'] != 0):
-            self.person.setX(self.person, + ((elapsed*25) * self.state_key['speed'] ))
+            self.person.setX(self.person, + ((elapsed*25) * self.state_key['speed_walk'] ))
 
 
         if ((self.state_key['up'] == 1) or (self.state_key['down'] == 1)):
